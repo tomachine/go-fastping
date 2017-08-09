05dbfd18acd5ddea3649409cf22ad5fc27777627
Anton Skorochod <anton@nsl.cz>
2017-08-09 16:04:44 +0200

    Example command

---
+++ cmd/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/kanocz/go-fastping"
+)
+
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Printf("Usage: %s <IP> <count>\n", os.Args[0])
+		return
+	}
+
+	count, err := strconv.ParseUint(os.Args[2], 10, 64)
+	if nil != err {
+		log.Fatalln("Error parsing count:", err)
+	}
+
+	p := fastping.NewPinger()
+	err = p.AddIP(os.Args[1])
+	if nil != err {
+		log.Fatalln("Error adding ip:", err)
+	}
+
+	p.MaxRTT = time.Second
+
+	totalCount := 0
+	rttSumm := time.Duration(0)
+
+	for i := uint64(0); i < count; i++ {
+		result, err := p.Run(map[string]bool{})
+		if err != nil {
+			log.Fatalln("Pinger error:", err)
+		}
+		fmt.Printf("%v ", result[os.Args[1]])
+		if result[os.Args[1]] > 0 {
+			totalCount++
+			rttSumm += result[os.Args[1]]
+		}
+	}
+	fmt.Println(" done!")
+	fmt.Printf("Pings received %d/%d with avg rtt = %v\n", totalCount, count, rttSumm/time.Duration(totalCount))
+}
